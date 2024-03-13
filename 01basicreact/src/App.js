96d8835af76dad2fbfb8f5e2e57454ca36dd7566
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2024-03-13 23:41:51 +0530

    2 projects created using create-react-app and vite react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Visit{" "}
+        <a href="https://techdevguide.withgoogle.com/paths/" target="blank">
+          DSA with Google
+        </a>{" "}
+        to learn DSA from google.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>This is a vite + react project</h1>
+    </>
+  );
+}
+
+export default App;
