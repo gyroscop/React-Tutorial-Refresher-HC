d01bf2b0e18fd44e01a5722455fc69f20445013e
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2024-03-13 23:53:30 +0530

    Custom App.jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>This is a vite + react project</h1>
---
+++ 01vitereact/src/life.jsx
@@ -0,0 +1,12 @@
+function Life() {
+  return (
+    <>
+      <h1>
+        {" "}
+        If you think you are in hell , than walk the place like you own it
+      </h1>
+    </>
+  );
+}
+
+export default Life;
