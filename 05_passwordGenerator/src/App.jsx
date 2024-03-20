1cd1c264ae836f8b0ee45648001485a8d64a0c5f
Dibya Jyoti Sarmah <dibyasarmah.work@gmail.com>
2024-03-21 01:39:36 +0530

    05_password_gen_v2

--- 05_passwordGenerator/src/App.jsx
+++ 05_passwordGenerator/src/App.jsx
@@ -1,16 +1,120 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 
 function App() {
   const [length, setlength] = useState(8);
-  const [numberAllowed, setnumberAllowed] = useState(false);
-  const [chatAllowed, setcharAllowed] = useState(false);
-  const [password, setPassword] = useState();
+  let [numberAllowed, setnumberAllowed] = useState(false);
+  const [charAllowed, setcharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  //useRef
+
+  const passwordRef = useRef(null);
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYSabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) {
+      str += "01234567789";
+    }
+    if (charAllowed) str += "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+
+    for (let i = 0; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, numberAllowed, charAllowed, passwordGenerator]);
+
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
+
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+      {/* <h1 className="text-4xl text-center text-white mb-10">{password}</h1>
+      <button
+        className="text-3xl text-center text-white"
+        onClick={() => {
+          passwordGenerator();
+        }}
+      >
+        Password Generator
+      </button> */}
+
+      <div className="w-screen max-w-md shadow-md rounded-lg px-4 py-3  bg-gray-800 text-orange-500">
+        <h1 className="text-white text-center my-3 text-3xl">
+          Password generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+            ref={passwordRef}
+          />
+          <button
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={copyPasswordToClipboard}
+          >
+            copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                {
+                  setlength(e.target.value);
+                }
+              }}
+            />
+            <label>Length : {length}</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              className="cursor-pointer"
+              onChange={() => {
+                setnumberAllowed((prev) => !prev);
+              }}
+            />
+            <label>Numbers</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              className="cursor-pointer"
+              onChange={() => {
+                setcharAllowed((prev) => !prev);
+              }}
+            />
+            <label>Characters</label>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
